ed0990864495b2df31ba06475ff89ce24ebdb2d4
InKryption <inkryption07@gmail.com>
2022-06-09 07:24:59 +0200

    small build.zig utility

--- build.zig
+++ build.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
 
+pub fn getPackage(b: *std.build.Builder, name: []const u8) std.build.Pkg {
+    return std.build.Pkg{
+        .name = name,
+        .source = .{ .path = b.pathJoin(std.fs.path.dirname(@src().file).?, "src/main.zig") },
+        .dependencies = null,
+    };
+}
+
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
