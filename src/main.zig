5818a55ebf4a15bd40df6f16a8e824845839641e
InKryption <inkryption07@gmail.com>
2022-07-23 17:09:46 +0200

    update main.zig to reflect removal of util.zig

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-const util = @import("util.zig");
-
 pub const signature: [8]u8 = [8]u8{ 137, 80, 78, 71, 13, 10, 26, 10 };
 pub const chunk = @import("chunk/chunk.zig");
 
