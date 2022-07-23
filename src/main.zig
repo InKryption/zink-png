9d8656a787718e666cf16e6d416e8fa756898a35
InKryption <inkryption07@gmail.com>
2022-07-23 15:58:03 +0200

    delete util.zig

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const builtin = @import("builtin");
 
 const util = @import("util.zig");
 
-pub const signature: [8]u8 = .{ 137, 80, 78, 71, 13, 10, 26, 10 };
+pub const signature: [8]u8 = [8]u8{ 137, 80, 78, 71, 13, 10, 26, 10 };
 pub const chunk = @import("chunk/chunk.zig");
 
 comptime {
