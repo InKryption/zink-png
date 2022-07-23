70f09ab2fcc991e28051672405799d510f1512e3
InKryption <inkryption07@gmail.com>
2022-07-23 19:18:48 +0200

    Add more comprehensive testing for PLTE conversion to-and-from bytes, fixing silly bug; also use an array instead of a struct for the entries

--- src/chunk/PLTE.zig
+++ src/chunk/PLTE.zig
@@ -4,7 +4,10 @@ const PLTE = @This();
 entries_buf: [256]Entry,
 count_minus_one: u8,
 
-pub const Entry = extern struct { r: u8, g: u8, b: u8 };
+/// r: entry[0]
+/// g: entry[1]
+/// b: entry[2]
+pub const Entry = [3]u8;
 pub fn count(plte: PLTE) std.math.IntFittingRange(1, 256) {
     return @as(std.math.IntFittingRange(1, 256), plte.count_minus_one) + 1;
 }
@@ -16,9 +19,7 @@ pub const RawBytes = std.BoundedArray(u8, 256 * 3);
 
 pub fn toBytes(plte: PLTE) RawBytes {
     var result = RawBytes{};
-    for (plte.entries()) |entry| {
-        result.appendSlice(&[_]u8{ entry.r, entry.g, entry.b }) catch unreachable;
-    }
+    result.appendSliceAssumeCapacity(std.mem.sliceAsBytes(plte.entries()));
     return result;
 }
 
@@ -28,13 +29,15 @@ pub const FromBytesResult = union(enum) {
     missing_two_bytes: MissingTwoBytes,
 
     pub const MissingOneByte = struct {
-        full: std.BoundedArray(Entry, 255),
+        full: Full,
         trailing: Trailing,
+        pub const Full = std.BoundedArray(Entry, 255);
         pub const Trailing = struct { r: u8, g: u8 };
     };
     pub const MissingTwoBytes = struct {
-        full: std.BoundedArray(Entry, 255),
+        full: Full,
         trailing: Trailing,
+        pub const Full = std.BoundedArray(Entry, 255);
         pub const Trailing = struct { r: u8 };
     };
 
@@ -57,11 +60,6 @@ pub fn fromBytes(bytes: RawBytes) FromBytesResult {
     var result_entries = std.BoundedArray(Entry, 256){};
     const used_bytes_len = bytes.len - (bytes.len % 3);
 
-    comptime std.debug.assert(@typeInfo(Entry).Struct.layout == .Extern);
-    comptime std.debug.assert(@typeInfo(Entry).Struct.fields.len == 3);
-    comptime std.debug.assert(std.mem.eql(u8, @typeInfo(Entry).Struct.fields[0].name, "r"));
-    comptime std.debug.assert(std.mem.eql(u8, @typeInfo(Entry).Struct.fields[1].name, "g"));
-    comptime std.debug.assert(std.mem.eql(u8, @typeInfo(Entry).Struct.fields[2].name, "b"));
     result_entries.appendSliceAssumeCapacity(std.mem.bytesAsSlice(Entry, bytes.constSlice()[0..used_bytes_len]));
 
     return switch (bytes.len - used_bytes_len) {
@@ -69,17 +67,17 @@ pub fn fromBytes(bytes: RawBytes) FromBytesResult {
             .entries_buf = result_entries.buffer,
             .count_minus_one = @intCast(u8, result_entries.len - 1),
         } },
-        1 => FromBytesResult{ .missing_one_byte = FromBytesResult.MissingOneByte{
+        1 => FromBytesResult{ .missing_two_bytes = FromBytesResult.MissingTwoBytes{
+            .full = std.BoundedArray(Entry, 255).fromSlice(result_entries.constSlice()) catch unreachable,
+            .trailing = .{
+                .r = bytes.get(bytes.len - 1),
+            },
+        } },
+        2 => FromBytesResult{ .missing_one_byte = FromBytesResult.MissingOneByte{
             .full = std.BoundedArray(Entry, 255).fromSlice(result_entries.constSlice()) catch unreachable,
             .trailing = .{
                 .r = bytes.get(bytes.len - 2),
                 .g = bytes.get(bytes.len - 1),
-            },
-        } },
-        2 => FromBytesResult{ .missing_two_bytes = FromBytesResult.MissingTwoBytes{
-            .full = std.BoundedArray(Entry, 255).fromSlice(result_entries.constSlice()) catch unreachable,
-            .trailing = .{
-                .r = bytes.get(bytes.len - 1),
             },
         } },
         else => unreachable,
@@ -87,16 +85,35 @@ pub fn fromBytes(bytes: RawBytes) FromBytesResult {
 }
 
 test "PLTE" {
-    var plte = comptime PLTE.fromBytes(PLTE.RawBytes.fromSlice(std.mem.asBytes(&[_][3]u8{
+    const correct_bytes = @bitCast([8 * 3]u8, [_][3]u8{
         //  r    g    b
         .{ 000, 000, 000 },
         .{ 255, 000, 000 },
+
         .{ 000, 255, 000 },
         .{ 000, 000, 255 },
+
         .{ 000, 255, 255 },
         .{ 255, 000, 255 },
+
         .{ 255, 255, 000 },
         .{ 255, 255, 255 },
-    })) catch @panic("Oof, didn't meant to do that")).ok;
+    });
+
+    var plte = PLTE.fromBytes(PLTE.RawBytes.fromSlice(correct_bytes[0..]) catch @panic("Oof, didn't meant to do that")).ok;
     try std.testing.expectEqual(plte, PLTE.fromBytes(plte.toBytes()).unwrap() catch @panic("Something went wrong there."));
+
+    try std.testing.expectEqual(PLTE.FromBytesResult{
+        .missing_one_byte = PLTE.FromBytesResult.MissingOneByte{
+            .full = PLTE.FromBytesResult.MissingOneByte.Full.fromSlice(plte.entries()) catch unreachable,
+            .trailing = .{ .r = 42, .g = 7 },
+        },
+    }, PLTE.fromBytes(PLTE.RawBytes.fromSlice(&correct_bytes ++ [_]u8{ 42, 7 }) catch unreachable));
+
+    try std.testing.expectEqual(PLTE.FromBytesResult{
+        .missing_two_bytes = PLTE.FromBytesResult.MissingTwoBytes{
+            .full = PLTE.FromBytesResult.MissingTwoBytes.Full.fromSlice(plte.entries()) catch unreachable,
+            .trailing = .{ .r = 42 },
+        },
+    }, PLTE.fromBytes(PLTE.RawBytes.fromSlice(&correct_bytes ++ [_]u8{42}) catch unreachable));
 }
